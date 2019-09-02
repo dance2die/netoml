710a166d7c16c1c87f9fd20d4feba69bc1fe78e7
Sung M. Kim <sungmkim80@gmail.com>
2019-09-01 23:34:06 -0400

    Processing property check passes!

--- src/index.ts
+++ src/index.ts
@@ -69,7 +69,44 @@ const toJson: toJson = async options => {
     throw Error(`Could not find the site matching "${name}"!`)
 
   const [site] = sites;
-  const { build_settings: buildSettings, processing_settings: processing } = site;
+  const { build_settings: buildSettings, processing_settings } = site;
+
+  const defaultProcessing = {
+    css: {
+      bundle: true,
+      minify: true
+    },
+    js: {
+      bundle: true,
+      minify: true
+    },
+    images: {
+      optimize: true,
+      compress: true
+    },
+    html: {
+      pretty_urls: true
+    },
+    skip_processing: true
+  }
+  const processing = Object.assign(defaultProcessing, {
+    css: {
+      bundle: processing_settings.css.bundle,
+      minify: processing_settings.css.minify
+    },
+    js: {
+      bundle: processing_settings.js.bundle,
+      minify: processing_settings.js.minify
+    },
+    images: {
+      optimize: processing_settings.images.optimize,
+      compress: processing_settings.images.compress || false,
+    },
+    html: {
+      pretty_urls: processing_settings.html.pretty_urls
+    },
+    skip_processing: processing_settings.skip,
+  })
 
   const build: Build = {
     base: buildSettings.base,
