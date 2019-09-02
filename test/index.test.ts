368e4ac6148f966f9619ead3ccb72bc44d504974
Sung M. Kim <sungmkim80@gmail.com>
2019-09-01 20:01:09 -0400

    Removed unused commented out code

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,14 +1,4 @@
 import auth from '../src/auth'
-// import * as Comnmand from "@netlify/cli-utils"
-
-// console.log(`Comnmand`, Comnmand)
-// import { sum } from '../src';
-// describe('blah', () => {
-//   it('works', () => {
-//     expect(sum(1, 1)).toEqual(2);
-//   });
-// });
-
 
 describe('Authentication', () => {
   test('user is logged in', () => {
